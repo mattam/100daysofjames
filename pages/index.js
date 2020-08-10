7c947d2554f3773e3d3c1b97f79dbb2b5c0ef6f4
Matthew Chan <mattam@gmail.com>
2020-08-09 23:55:22 -0700

    Changing the front page UI to link to the day's PR experiments

--- pages/index.js
+++ pages/index.js
@@ -13,37 +13,16 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>#100daysofJames</h1>
 
-        <p className={styles.description}>Read through James and experiment with ways of learning it</p>
+        <p className={styles.description}>
+          Read through James and experiment with ways of learning it
+        </p>
 
-        <Link href="/james/1">
-          <a className={styles.card}>
-            <h3>James 1 &rarr;</h3>
-          </a>
-        </Link>
-
-        <Link href="/james/2">
-          <a className={styles.card}>
-            <h3>James 2 &rarr;</h3>
-          </a>
-        </Link>
-
-        <Link href="/james/3">
-          <a className={styles.card}>
-            <h3>James 3 &rarr;</h3>
-          </a>
-        </Link>
-
-        <Link href="/james/4">
-          <a className={styles.card}>
-            <h3>James 4 &rarr;</h3>
-          </a>
-        </Link>
-
-        <Link href="/james/5">
-          <a className={styles.card}>
-            <h3>James 5 &rarr;</h3>
-          </a>
-        </Link>
+        <a
+          href="https://100daysofjames-git-matt-testvercelbuild.mattam.vercel.app/james/1"
+          className={styles.card}
+        >
+          <h3>Day 1 &rarr;</h3>
+        </a>
       </main>
     </div>
   );
