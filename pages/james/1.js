2c9bd0667e1ddd99e9579bb992eab3f0687f5650
Matthew Chan <mattam@gmail.com>
2020-08-12 12:04:57 -0700

    Changing the chapter pages to use the shared passage rendering component
    and fixing up formatting tags at the same time.

--- pages/james/1.js
+++ pages/james/1.js
@@ -1,51 +1,21 @@
-import styles from "../../styles/Home.module.css";
-import BottomNav from "../../components/bottomNav";
+import renderPassage from "../../components/passage";
+
+const chapterNum = 1;
 
 export async function getStaticProps() {
   const res = await fetch(
-    "https://labs.bible.org/api/?passage=James%201&type=json&formatting=para"
+    `https://labs.bible.org/api/?passage=James%20${chapterNum}&type=json&formatting=para`
   );
-  const verses = await res.text();
+  const rawVerses = await res.text();
 
   return {
     props: {
-      verses,
+      rawVerses,
     },
   };
 }
 
-function renderVerse(verse) {
-  let hasPara = false;
-  if (verse.text.includes(`<p class="bodytext">`)) {
-    verse.text = verse.text.replace('<p class="bodytext">', "");
-  }
-  if (verse.text.includes(`</p>`)) {
-    verse.text = verse.text.replace("</p>", "");
-    hasPara = true;
-  }
-
-  return (
-    <span key={verse.verse}>
-      {verse.title && <h3>{verse.title}</h3>}
-      <span>{verse.text}</span>
-      {hasPara && <h3></h3>}
-    </span>
-  );
-}
-
-function renderPassage(rawVerses) {
+export default function James1({ rawVerses }) {
   const verses = JSON.parse(rawVerses);
-
-  return (
-    <div className={styles.container}>
-      <div className={styles.passage}>
-        {verses.map((verse) => renderVerse(verse))}
-      </div>
-      <BottomNav page={1} />
-    </div>
-  );
-}
-
-export default function James1({ verses }) {
-  return renderPassage(verses);
+  return renderPassage(verses, chapterNum);
 }
