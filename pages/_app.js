25062cb39e3d2b67ee49aa8cdac1f5e054ba05d4
Matthew Chan <mattam@gmail.com>
2020-08-15 13:33:34 -0700

    Adding in google analytics into the Nextjs app, fixed #15

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import Router from "next/router";
+import * as gtag from "../lib/gtag";
 import "../styles/globals.css";
 import { SettingsProvider } from "../data/settingsContext";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    Router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return (
     <SettingsProvider>
       <Component {...pageProps} />
