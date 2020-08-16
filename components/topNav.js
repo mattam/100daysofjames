3492c369168a62532d04d19fc252571840d4c11a
Matthew Chan <mattam@gmail.com>
2020-08-16 01:16:48 -0700

    Adding in a toggle for the info panel

--- components/topNav.js
+++ components/topNav.js
@@ -1,11 +1,22 @@
 import { useContext } from "react";
 import styles from "../styles/Home.module.css";
+import { useState } from "react";
 import { SettingsContext } from "../data/settingsContext";
 
 export default function TopNav() {
   const [settings, setSettings] = useContext(SettingsContext);
+  const [showInfo, setShowInfo] = useState(false);
+  console.log("showInfo:", showInfo);
   return (
     <div className={styles.topNav}>
+      <div
+        className={styles.navCircle}
+        onClick={() => {
+          setShowInfo(!showInfo);
+        }}
+      >
+        <h2>i</h2>
+      </div>
       <div
         className={styles.navCircle}
         onClick={() => {
